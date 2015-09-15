f4e8fcf5311c065614b6dce119e8dc19aba6643f
josenberg <jose.philipe@hotmail.com>
2015-09-14 22:17:46 -0300

    changing the default URL in the config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://josenberg.com.br',
         mail: {},
         database: {
             client: 'sqlite3',
